73ec42845bfaee93a41b725c4b9ab767887980f0
Jacob Sipple <sipple3@gmail.com>
2019-01-04 13:03:02 -0500

    added images and css

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+
+// on click character you want all others move to fight area
+$(".char").on("click", function(event) {
+
+})
+
+// click character you want to fight
+
+// on click of button have character fight opponent loses health first if dies does not hit back alse display damage done and recieved
+
+// also need to update attack damage each time for player but not for opponent
+
