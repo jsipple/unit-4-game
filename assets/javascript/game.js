0daee634908d025dcec1c8434342afbbca3f5c08
Jacob Sipple <sipple3@gmail.com>
2019-01-05 01:11:57 -0500

    fixed it so that it puts two fighting each other(can have multiple not sure if i should change that or not also made button visible when two fighting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,51 @@
 
+let rancor = {
+  health: 500,
+  attack: 30,
+  counterAttack: 40
+}
+
+let obiWan = {
+  health: 250,
+  attack: 15,
+  counterAttack: 15
+}
+
+let bobaFett = {
+  health: 200,
+  attack: 30,
+  counterAttack: 40
+}
+
+let darthMaul = {
+  health: 300,
+  attack: 25,
+  counterAttack: 25
+}
+
+
 $(".pick").on("click", function(event) {
   $(this).addClass("main-character")
-  // need to figure out how to remove all of pick seemso to take out but still running for some reason
-  $("div").removeClass("pick").addClass("fight")
-  $(".main-character").appendTo("#main-character")
-  $(".main-character").removeClass("fight")
-  $("#instr").text("Choose a character to fight")
-})
+  $("section").removeClass("pick").addClass("fight");
+  $(".main-character").appendTo("#main-character");
+  $(".main-character").removeClass("fight");
+  $("#instr").text("Choose a character to fight");
+  // why does this need to be in here rather than after(doesn't work if after)
+  $(".fight").on("click", function(event) {
+    $(this).removeClass("fight").addClass("fighting");
+    $(".fighting").appendTo("#fighting");
+    $("#fight").show();
+  });
+});
 
-$(".fight").on("click", function(event) {
-  $(this).removeClass("fight").addClass("fighting")
-  $(".fighting").appendTo("#fighting")
-  $("#fight").show();
-})
+
+
+
+function Attack() {
+  let i = 1
+
+  i++
+}
 
 // // click character you want to fight should put it into id fighter should also make button id fight visable
 
