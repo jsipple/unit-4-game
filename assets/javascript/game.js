3e42f74985abb853a6816433e98b13d20950dff8
Jacob Sipple <sipple3@gmail.com>
2019-01-04 19:01:53 -0500

    have it so that it moves character and takes out class but still answering to old class even though it is being removed need to fix also started on making character you will fight

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,22 @@
 
-// on click character you want all others move to fight area
-$(".character").on("click", function(event) {
-    var $this = $(this); // this is just for performance
-  if(!$this.hasClass('character'))
-    $('.character').toggleClass(".mainCharacter");
-    // this empties the array need to make it so it does not empty one clicked so maybe change something idk
-    $(".char").empty();
-});
+$(".pick").on("click", function(event) {
+  $(this).addClass("main-character")
+  // need to figure out how to remove all of pick seemso to take out but still running for some reason
+  $("div").removeClass("pick").addClass("fight")
+  $(".main-character").appendTo("#main-character")
+  $(".main-character").removeClass("fight")
+  $("#instr").text("Choose a character to fight")
+})
 
-// click character you want to fight
+$(".fight").on("click", function(event) {
+  $(this).removeClass("fight").addClass("fighting")
+  $(".fighting").appendTo("#fighting")
+  $("#fight").show();
+})
 
-// on click of button have character fight opponent loses health first if dies does not hit back alse display damage done and recieved
+// // click character you want to fight should put it into id fighter should also make button id fight visable
 
-// also need to update attack damage each time for player but not for opponent
+// // on click of button have character fight opponent loses health first if dies does not hit back alse display damage done and recieved
+// // also have this log in probably instru the damage dealt and recieved
 
+// // // also need to update attack damage each time for player but not for opponent(says should double each time player attacks)
