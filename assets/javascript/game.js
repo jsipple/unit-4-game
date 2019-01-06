9f2d55c2829211ce38f2b170c4cb6f9594a810f7
Jacob Sipple <sipple3@gmail.com>
2019-01-05 19:56:58 -0500

    mostly working need to add defeat mechanic and make it so that the fighting person doesn't change the main character's stats

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,78 @@
-
-let rancor = {
+let players = {
+rancor : {
   health: 500,
   attack: 30,
   counterAttack: 40
-}
+},
 
-let obiWan = {
+obiWan: {
   health: 250,
   attack: 15,
   counterAttack: 15
-}
+},
 
-let bobaFett = {
+bobaFett: {
   health: 200,
   attack: 30,
   counterAttack: 40
-}
+},
 
-let darthMaul = {
+darthMaul: {
   health: 300,
   attack: 25,
   counterAttack: 25
 }
-
+}
+let multiplier = 1
+let mainhealth = 0;
+let character = ""
+let fighting = ""
+let fighterHealth = 0;
+let fighterCounterAttack = 0;
 
 $(".pick").on("click", function(event) {
   $(this).addClass("main-character")
   $("section").removeClass("pick").addClass("fight");
   $(".main-character").appendTo("#main-character");
   $(".main-character").removeClass("fight");
+  character = $(".main-character")[0].id;
+  // problem with this it's running for the person you fighting instead of first one chosen same thing with attack
+  mainHealth = players[character].health;
+  console.log(mainHealth)
   $("#instr").text("Choose a character to fight");
   // why does this need to be in here rather than after(doesn't work if after)
+  // they are also getting main-character class looks like still running pick but then running this
   $(".fight").on("click", function(event) {
     $(this).removeClass("fight").addClass("fighting");
+    // this removes main-character class look into ways of doing this without needing to add this
+    $(this).removeClass("main-character")
     $(".fighting").appendTo("#fighting");
     $("#fight").show();
+    fighting = $(".fighting")[0].id;  
+    fighterHealth = players[fighting].health;
+    fighterCounterAttack = players[fighting].counterAttack;
   });
 });
 
 
 
 
-function Attack() {
-  let i = 1
 
-  i++
+function Attack() {
+  // right now sets back each time
+  // add a value to the id
+  // need to grab the p id with this
+  // this sets it back each time
+  let mainAttack = players[character].attack * multiplier;
+  // this sets it back each time
+  fighterHealth = fighterHealth - mainAttack;
+  $("#fighting .health").text(fighterHealth)
+  if (fighterHealth > 0) {
+    console.log(mainHealth)
+  mainHealth = mainHealth - fighterCounterAttack
+  }
+  $("#main-character .health").text(mainHealth);
+  multiplier++
 }
 
 // // click character you want to fight should put it into id fighter should also make button id fight visable
